b74e795b3c8c5806b059215a14381e5b556b4d5c
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2025-01-27 04:07:33 -0500

    Add CRUD functionality for Spaces and update Locations template links

--- Aplicaciones/Expocicion/urls.py
+++ Aplicaciones/Expocicion/urls.py
@@ -3,9 +3,21 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('location', views.listLocation, name='location'),
-    path('locationNew', views.locationNew, name='locationNew'),
-    path('newLocation', views.newLocation, name='newLocation'),
-    path('listLocationForId/<id>', views.listLocationForId, name='listLocationForId'),
-    path('update_location', views.update_location, name='update_location'),
+    path('location/', views.listLocation, name='location'),
+    path('locationNew/', views.locationNew, name='locationNew'),
+    path('newLocation/', views.newLocation, name='newLocation'),
+    path('listLocationForId/<id>/', views.listLocationForId, name='listLocationForId'),
+    path('update_location/', views.update_location, name='update_location'),
+    path('deleteLocation/<id>', views.deleteLocation , name='deleteLocation'), 
+    
+    path('listSpace/', views.listSpace, name='listSpace'),
+    path('spaceNew', views.spaceNew, name='spaceNew'),
+    path('saveSpace/', views.saveSpace, name='saveSpace'),
+    path('listSpaceForId/<id>/', views.listSpaceForId, name='listSpaceForId'),
+    path('updateSpace/', views.updateSpace, name='updateSpace'),
+    path('deleteSpace/<id>', views.deleteSpace , name='deleteSpace'), 
+    
+    
+    
+    
 ]
--- Aplicaciones/Expocicion/views.py
+++ Aplicaciones/Expocicion/views.py
@@ -19,7 +19,6 @@ def newLocation(request):
     city = request.POST.get('city')
     
     location = Location.objects.create(name=name, latitude=latitude, longitude=longitude, city=city)
-    location.save()
     return redirect('location')
 
 def listLocationForId(request, id):
@@ -38,6 +37,57 @@ def update_location(request):
     
     location.save()
     return redirect('location')
+
+def deleteLocation(request, id):
+    location = Location.objects.get(id=id)
+    location.delete()
+    
+    return redirect('location')
+
+def listSpace(request):
+    spaces = Space.objects.all()
+    return render(request, 'Spaces/index.html', {'spaces': spaces})
+
+def spaceNew(request):
+    locations = Location.objects.all()
+    return render(request, 'Spaces/newSpace.html', {'locations': locations})
+
+def saveSpace(request):
+    name = request.POST.get('name')
+    capacity = request.POST.get('capacity')
+    loc = request.POST.get('location')
+    location = Location.objects.get(id=loc)
+    
+    space = Space.objects.create(name=name, capacity=capacity, location=location)
+    
+    return redirect('listSpace')
+
+def listSpaceForId(request, id):
+    space = Space.objects.get(id=id)
+    locations = Location.objects.all()
+    return render(request, 'Spaces/updateSpace.html', {'space': space, 'locations': locations})
+
+def updateSpace(request):
+    id = request.POST.get('id')
+    space = Space.objects.get(id=id)
+    space.name = request.POST.get('name')
+    space.capacity = request.POST.get('capacity')
+    space.location = Location.objects.get(id=request.POST.get('location'))
+    
+    space.save()
+    return redirect('listSpace')
+
+def deleteSpace(request, id):
+    space = Space.objects.get(id=id)
+    space.delete()
+    
+    return redirect('listSpace')
+    
+
+
+    
+
+
 
     
     
